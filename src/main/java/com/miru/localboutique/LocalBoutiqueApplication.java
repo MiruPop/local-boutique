036903438e29785e2b87ab5189b382ea3fa03208
Miruna Popescu <miru.popescu@yahoo.fr>
2022-04-13 15:45:13 +0200

    base structure

---
+++ src/main/java/com/miru/localboutique/LocalBoutiqueApplication.java
@@ -0,0 +1,13 @@
+package com.miru.localboutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalBoutiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalBoutiqueApplication.class, args);
+    }
+
+}
